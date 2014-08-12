e914024fad4fc28f94a71a9b56d47404400a5d9e
uma shankar <umashankar@wetwaresys.com>
2014-08-12 13:30:40 -0700

    control

--- VehicleBooking/VehicleBooking/VehicleBookingMain/VehicleBookingMain/Master/CountryMasterView.xaml.cs
+++ VehicleBooking/VehicleBooking/VehicleBookingMain/VehicleBookingMain/Master/CountryMasterView.xaml.cs
@@ -21,12 +21,63 @@ namespace VehicleBookingMain.Master
     /// </summary>
     public partial class CountryMasterView : UserControl
     {
+        DataGrid dataGrid1;
         public CountryMasterView()
         {
             InitializeComponent();
+            dataGrid1 = new DataGrid();
+            dataGrid1.AutoGenerateColumns = true;
+            dataGrid1.HorizontalAlignment = System.Windows.HorizontalAlignment.Right;
+            dataGrid1.Margin = new Thickness(10);
+            dataGrid1.VerticalAlignment = System.Windows.VerticalAlignment.Top;
+            grdField.Children.Add(dataGrid1);
+            Grid.SetColumn(dataGrid1, 1);
             //BindGrid();
+            loadField();
         }
-        private void button1_Click(object sender, RoutedEventArgs e)
+        void loadField()
+        {
+            StackPanel st = new StackPanel();
+            Xceed.Wpf.Toolkit.WatermarkTextBox water = new Xceed.Wpf.Toolkit.WatermarkTextBox();
+            water.Width = 200;
+            water.Margin = new Thickness(0, 10, 0,10);
+            water.Watermark = "Country Code";
+            st.Children.Add(water);
+            Xceed.Wpf.Toolkit.WatermarkTextBox water2 = new Xceed.Wpf.Toolkit.WatermarkTextBox();
+            water2.Width = 200;
+            water2.Watermark = "Country Name";
+            water2.Margin = new Thickness(0, 10,0,10);
+            st.Children.Add(water2);
+            Xceed.Wpf.Toolkit.WatermarkTextBox water3 = new Xceed.Wpf.Toolkit.WatermarkTextBox();
+            water3.Width = 200;
+            water3.Watermark = "ISD Code";
+            water3.Margin = new Thickness(0, 10, 0, 10);
+            st.Children.Add(water3);
+            StackPanel stpanal = new StackPanel();
+            stpanal.Orientation = Orientation.Horizontal;
+            stpanal.Margin = new Thickness(20);
+            stpanal.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
+            Button bt = new Button();
+            bt.Name = "btnSubmit";
+            bt.Click += bt_Click;
+            bt.Style = Application.Current.FindResource("MetroButtonSubmit") as Style;
+            
+           // bt.Margin = new Thickness(0, 10, 0, 10);
+            bt.Margin = new Thickness(0, 0, 10, 0);
+            Button btcncle = new Button();
+            btcncle.Style = Application.Current.FindResource("MetroButtonClose") as Style;
+
+            btcncle.Margin = new Thickness(30,0 , 0,0);
+            stpanal.Children.Add(bt);
+            stpanal.Children.Add(btcncle);
+           
+            st.Children.Add(stpanal);
+
+
+            grdField.Children.Add(st);
+        }
+
+        void bt_Click(object sender, RoutedEventArgs e)
         {
             CountryMasterDataService countryDs = new CountryMasterDataService();
             CountryMaster countryBL = new CountryMaster();
@@ -39,6 +90,10 @@ namespace VehicleBookingMain.Master
             countryBL = null;
             countryList = null;
             countryDs = null;
+        }
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+           
         }
 
         private void BindGrid()
